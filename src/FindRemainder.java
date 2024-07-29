f294c82fbf7378fdc4158b3bd8ceafefba582f4c
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-07-29 19:38:55 +1000

    Get remainder from division program is added

--- src/FindRemainder.java
+++ src/FindRemainder.java
@@ -1,14 +1,13 @@
 import java.util.regex.Pattern;
 
-public class DivideBy3Quotient {
+public class FindRemainder {
     public static void main(String[] args){
-        String[][] states={{"|||","a"},{"a|","a"},{"baaaaaaaaaa","ab"},{"baaaaaaaaa","9"},
-                {"baaaaaaaa","8"},{"baaaaaaa","7"},{"baaaaaa","6"},{"baaaaa","5"},{"baaaa","4"},
-                {"baaa","3"},{"baa","2"},{"ba","1"},{"b","0"},{"a","ba"}};
-        String str="||||||||";
-        System.out.println(divide3Quotient(str,states));
+        String[][] states={{"|:0",":0"},{":0","0."},{":","abcd"},{"d|","Dd"},{"d",""},{"|D","D|"},{"cD","|c|"},{"|a","a|"},{"|b|","b"},{"bc","bcd"},
+                {"ab","e"},{"e|","|e"},{"|ec|","ec"},{"ec","."}};
+        String str="||||||||:|||";
+        System.out.println(dividedRemainder(str,states));
     }
-    public static String divide3Quotient(String namEnter, String[][] states){
+    public static String dividedRemainder(String namEnter, String[][] states){
         boolean shouldRepeat = true;
         while(shouldRepeat){
             shouldRepeat=false;
