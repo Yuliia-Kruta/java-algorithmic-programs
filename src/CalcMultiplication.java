f272dcc4100d8e51fb79ff31acac1147c6789da6
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-07-29 19:42:33 +1000

    Gitignore updated

--- src/CalcMultiplication.java
+++ src/CalcMultiplication.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class CalcMultiplication {
+import java.util.regex.Pattern;
+
+public class CalcMultiplication {
+    public static void main(String[] args) {
+        String[][] states = {{"a|", "|ba"},{"a", ""},{"b|", "|b"},{"|*", "*a"},{"*", "c"},{"c|", "c"},{"cb", "|c"},{"c", "."}};
+        String str = "||||*|||";
+        System.out.println(rec(str, states));
+    }
+    public static String rec(String namEnter, String[][] states){
+        boolean shouldRepeat = true;
+        System.out.println(namEnter);
+        if (shouldRepeat = false){
+            return namEnter;
+        }
+        else{
+            shouldRepeat=false;
+            for (String[] state : states) {
+                if (namEnter.contains(state[0])) {
+                    shouldRepeat = true;
+                    namEnter = namEnter.replaceFirst(Pattern.quote(state[0]), state[1]);
+                    if (state[1].contains(".")) {
+                        return namEnter.replace(".", "");
+                    }
+                    return rec(namEnter, states);
+                }
+            }
+            return rec(namEnter, states);
+        }
+    }
 }
