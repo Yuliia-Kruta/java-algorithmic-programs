0c0587127716bb89251fbd667a4e6e4936aef18f
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-07-29 19:26:37 +1000

    Calculate expression 2 program is added

---
+++ src/CalcExpression2.java
@@ -0,0 +1,27 @@
+import java.util.regex.Pattern;
+
+public class CalcExpression2 {
+    public static void main(String[] args){
+        String[][] states={{"+",""},{"|-|","-"},{")",""},{"(",""},{"|-","|."},{"-|","*|"},{"*|","*"},{"*","Помилка, функція невизначена!."},{"-","0."}};
+        String str="||||||||-(||||+||)";
+        System.out.println(expression2(str,states));
+    }
+    public static String expression2(String namEnter, String[][] states){
+        boolean shouldRepeat = true;
+        while(shouldRepeat){
+            shouldRepeat=false;
+            System.out.println(namEnter);
+            for (String[] state : states) {
+                if (namEnter.contains(state[0])) {
+                    shouldRepeat = true;
+                    namEnter = namEnter.replaceFirst(Pattern.quote(state[0]), state[1]);
+                    if (state[1].contains(".")) {
+                        return namEnter.replace(".", "");
+                    }
+                    break;
+                }
+            }
+        }
+        return namEnter;
+    }
+}
